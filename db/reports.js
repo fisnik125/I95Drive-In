d517cff84832e2cedac2c1eaffc17241f6f6aab3
Peter Mooney <peter.mooney90@gmail.com>
2018-05-02 14:27:35 -0400

    Mongo-itize Movies By Profit Report

--- db/reports.js
+++ db/reports.js
@@ -9,9 +9,36 @@ const postgresCommands = {
 }
 
 const mongoCommands = {
-  moviesByProfit: async (db, params) => (
-    null
-  ),
+  moviesByProfit: async (db, params) => {
+    return db.collection('transactions').aggregate([{
+      $lookup: {
+        from: 'showtimes',
+        localField: 'transactionableId',
+        foreignField: '_id',
+        as: 'showtimes'
+      }
+    }, {
+      $lookup: {
+        from: 'movies',
+        localField: 'showtimes.0.movieId',
+        foreignField: '_id',
+        as: 'movie'
+      }
+    }, {
+      $unwind: "$showtimes"
+    }, {
+      $group: {
+        _id: "$movie.title",
+        value: { $sum: { $multiply: [ "$quantity", "$showtimes.price" ] } }
+      }
+    }, {
+      $sort: { value: -1 }
+    }, {
+      $unwind: "$_id"
+    }, {
+      $project: { name: "$_id", value: 1, _id: 0 }
+    }]).toArray();
+  },
 }
 
 const commands = process.env.MONGO === 'true' ? mongoCommands : postgresCommands;
