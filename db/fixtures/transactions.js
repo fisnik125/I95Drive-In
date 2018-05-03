348e0a835e149143b5e7b8a567b81434903c7efd
Peter Mooney <peter.mooney90@gmail.com>
2018-05-03 08:58:01 -0400

    Add fixtures for concessions

--- db/fixtures/transactions.js
+++ db/fixtures/transactions.js
@@ -4,6 +4,7 @@ const mongodb = require('mongodb');
 const Users = require('../Users');
 const Transactions = require('../Transactions');
 const Showtimes = require('../Showtimes');
+const Concessions = require('../Concessions');
 
 const dbName = 'i95drivein';
 
@@ -16,27 +17,41 @@ const dbName = 'i95drivein';
   await mongo.collection('transactions').remove({});
 
   const users = await postgres.query(Users.all);
-  const showtimes = await postgres.query(Showtimes.all)
+  const showtimes = await postgres.query(Showtimes.all);
+  const concessions = await postgres.query(Concessions.all);
 
   const records = await users.rows.map(async user => {
     const transactions = numTransactions();
 
     return Array(transactions).fill().map(async transaction => {
       const userEmail = user.email;
-      const { id: transactionableId, start_date: startDate, end_date: endDate } = randomShowtime(showtimes.rows);
+      const transactionableType = showtimeOrConcession();
       const quantity = randomQuantity();
-      const transactionableType = 'showtimes';
 
-      console.log(`Inserting Transaction for ${userEmail}. Quantity: ${quantity}`);
-      await postgres.query(Transactions.insert, [userEmail, transactionableId, transactionableType, quantity]);
-      const mongoShowtime = await mongo.collection('showtimes').findOne({ startDate, endDate });
+      if (transactionableType === 'showtimes') {
+        const { id: transactionableId, start_date: startDate, end_date: endDate } = randomShowtime(showtimes.rows);
+        console.log(`Inserting Showtime Transaction for ${userEmail}. Quantity: ${quantity}`);
 
-      try {
+        await postgres.query(Transactions.insert, [userEmail, transactionableId, transactionableType, quantity]);
+        const mongoShowtime = await mongo.collection('showtimes').findOne({ startDate, endDate });
+
+        try {
+          await mongo.collection('transactions').insertOne({
+            userEmail, transactionableId: mongoShowtime._id, transactionableType, quantity
+          });
+        } catch (error) {
+          console.error('!!! Error inserting mongo transaction !!!:');
+        }
+      } else {
+        const { id: transactionableId, type } = randomConcession(concessions.rows);
+        console.log(`Inserting Concession Transaction for ${userEmail}. Quantity: ${quantity}`);
+
+        await postgres.query(Transactions.insert, [userEmail, transactionableId, transactionableType, quantity]);
+
+        const mongoConcession = await mongo.collection('concessions').findOne({ type });
         await mongo.collection('transactions').insertOne({
-          userEmail, transactionableId: mongoShowtime._id, transactionableType, quantity
+          userEmail, transactionableId: mongoConcession._id, transactionableType, quantity
         });
-      } catch (error) {
-        console.error('!!! Error inserting mongo transaction !!!:');
       }
     });
   });
@@ -48,9 +63,14 @@ const dbName = 'i95drivein';
   console.log("\n\n*** COMPLETE *** Press CTRL+C to Quit");
 })();
 
-const numTransactions = () => Math.floor(Math.random() * Math.floor(3)) // 0..3
+const numTransactions = () => Math.floor(Math.random() * Math.floor(8)) // 0..8
 const randomQuantity = () => Math.floor(Math.random() * Math.floor(5) + 1) // 1..5
+const showtimeOrConcession = () => Math.random() < .5 ? 'concessions' : 'showtimes';
 const randomShowtime = (showtimes) => {
   const randomIndex = Math.floor(Math.random() * Math.floor(showtimes.length));
   return showtimes[randomIndex];
+}
+const randomConcession = (concessions) => {
+  const randomIndex = Math.floor(Math.random() * Math.floor(concessions.length));
+  return concessions[randomIndex];
 }
